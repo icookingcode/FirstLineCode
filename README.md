64c5c76fcb3a8de8c052bdada47246895ed3cb80
guchaochao <guchaochao@huiyunit.com>
2020-04-27 17:32:11 +0800

    Kotlin语法练习

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # FirstLineCode
 第一行代码（第三版）
+##kotlin学习要点
+### 函数式API(Lambda表达式)
+语法结构  
+```
+完整结构：
+{参数1：参数类型,参数2：参数类型 ,..->函数体}
+参数类型大多数可省略：
+{参数1，参数2 -> 函数体}
+Lambda只有一个参数，可用it代替参数，只保留函数体：
+{it}
+```  
+*  当lambda表达式为函数的最后一个参数时，可将表达式移到括号外面；当仅有一个lambda表达式参数时，可将（）省略。  
+fun(lambda)   <=>  fun()lambda  <=>  fun lambda  
+### 空检测
+加!!像Java一样抛出空异常，加?可不做处理返回值为 null或配合?:做空判断处理  
+* obj?.do() 非空时执行  <=> if(obj!=null) obj.do()  
+* a = obj?:b  <=> a = if(obj!=null) obj else b
+* obj!!.do() 非空时执行，空时断言报错  <=> if(obj!=null) obj.do() else throws Exception
+
+### 默认参数
+```
+/**
+ * 默认参数 可选
+ */
+fun getUserInfo(name:String,age:Int ,sex:String="male",address:String)="姓名：$name  年龄：$age  性别：$sex  地址：$address"
+
+//调用 有默认参数的可不传,没有默认值则必传  若默认参数不在最后,则必传
+val res = getUserInfo("guchao",30,"male","焦作沁阳市")
+
+//若不想传默认参数，但其后仍有参数，可通过键值对方式传参
+val res = getUserInfo("guchao",30,address = "焦作沁阳市")
+```
---
+++ app/src/main/java/com/guc/firstlinecode/ktexercise/KotlinExercise.kt
@@ -0,0 +1,69 @@
+package com.guc.firstlinecode.ktexercise
+
+/**
+ * Created by guc on 2020/4/27.
+ * 描述：基本语法练习
+ */
+val plus: (Int, Int) -> Int = { a: Int, b: Int -> a + b } //Lambda表达式
+
+//除法
+val division: (Int, Int) -> Int? = { a: Int, b: Int -> if (b == 0) null else a / b }
+
+val plus2 = { a: Int, b: Int -> a + b } //可省略类型
+
+fun main(args: Array<String>) {
+    println("FirstLineCode")
+    val res = plus(3, 4)
+    println("3+4=$res")
+    val res4 = plus4(5, 4)
+    println("5+4=$res4")
+
+    val res5 = operation(5, 6) { p1, p2 -> p1 * p2 }
+    println("5*6=$res5")
+
+    val res6 = operation(5, 0, division)
+    println("5/0 = $res6")
+
+    val res7 = getStringLength("guchao")
+    val res8 = getStringLength(null)
+    println("guchao.length = $res7\nnull.length = $res8")
+
+    val res9 = getUserInfo("guchao", 30, address = "焦作沁阳市")
+    val res10 = getUserInfo("lily", 29, "female", "America")
+    println(res9)
+    println(res10)
+}
+
+fun plus3(a: Int, b: Int): Int {
+    return a + b
+}
+
+//plus3 简写 ：方法体只有一句时可用 = 直接返回
+fun plus4(a: Int, b: Int) = a + b
+
+/*
+Lambda 表达式作为参数
+ */
+fun operation(p1: Int, p2: Int, lambda: (Int, Int) -> Int?): Int? {
+    return lambda(p1, p2)
+}
+
+/**
+ * 获取字符串的长度
+ * s ==null  返回0
+ */
+fun getStringLength(s: String?): Int = s?.length ?: 0
+
+/**
+ * 获取字符串的长度
+ * s ==null  抛异常
+ */
+fun getStringLength2(s: String?): Int = s!!.length
+
+/**
+ * 默认参数 可选
+ */
+fun getUserInfo(name: String, age: Int, sex: String = "male", address: String) =
+    "姓名：$name  年龄：$age  性别：$sex  地址：$address"
+
+
