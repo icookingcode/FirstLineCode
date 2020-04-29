339e7b76acb07b612364b1a3fb045ffcb87accf0
guchaochao <guchaochao@huiyunit.com>
2020-04-29 15:02:44 +0800

    RecyclerView适配器修改

--- app/src/main/java/com/guc/firstlinecode/adapter/common/Commondapter4Rcv.kt
+++ app/src/main/java/com/guc/firstlinecode/adapter/common/Commondapter4Rcv.kt
@@ -1,19 +1,17 @@
 package com.guc.firstlinecode.adapter.common
 
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
 /**
  * Created by guc on 2020/4/29.
- * 描述：通用Rcv
+ * 描述：通用Rcv  可加载多种样式的Item
  */
-abstract class Commondapter4Rcv<T>(private val layoutId: Int, val datas: List<T>) :
+abstract class Commondapter4Rcv<T>(val datas: List<T>) :
     Adapter<ViewHolder4RecyclerView>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder4RecyclerView {
-        val root: View = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
-        return ViewHolder4RecyclerView(root)
+        return ViewHolder4RecyclerView(getRootView(parent, viewType))
     }
 
     override fun getItemCount(): Int = datas.size
@@ -25,6 +23,9 @@ abstract class Commondapter4Rcv<T>(private val layoutId: Int, val datas: List<T>
     override fun getItemViewType(position: Int): Int {
         return super.getItemViewType(position)
     }
+
+    //获取itemView
+    abstract fun getRootView(parent: ViewGroup, viewType: Int): View
 
     //数据绑定
     abstract fun bindData(
