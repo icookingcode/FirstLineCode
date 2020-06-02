a793802daf9751d91b80cb94fd7720419b280ad2
guchaochao <guchaochao@huiyunit.com>
2020-06-02 17:08:22 +0800

    LiveData

--- app/src/main/java/com/guc/firstlinecode/ui/jetpack/JetpackActivity.kt
+++ app/src/main/java/com/guc/firstlinecode/ui/jetpack/JetpackActivity.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.guc.firstlinecode.R
 import com.guc.firstlinecode.base.BaseActivity
 import com.guc.firstlinecode.ui.jetpack.lifecycle.LifecyclesActivity
+import com.guc.firstlinecode.ui.jetpack.lifedata.LiveDataActivity
 import com.guc.firstlinecode.ui.jetpack.vm.ViewModelActivity
 import com.guc.firstlinecode.utils.quickStartActivity
 import kotlinx.android.synthetic.main.activity_jetpack.*
@@ -21,12 +22,14 @@ class JetpackActivity : BaseActivity(), View.OnClickListener {
         titleLayout.title = "Jetpack 组件库"
         btnViewModel.setOnClickListener(this)
         btnLifecycle.setOnClickListener(this)
+        btnLiveData.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btnViewModel -> quickStartActivity<ViewModelActivity>(this) {}
             R.id.btnLifecycle -> quickStartActivity<LifecyclesActivity>(this) {}
+            R.id.btnLiveData -> quickStartActivity<LiveDataActivity>(this) {}
         }
     }
 }
---
+++ app/src/main/java/com/guc/firstlinecode/ui/jetpack/lifedata/DataViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.guc.firstlinecode.ui.jetpack.lifedata
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+/**
+ * Created by guc on 2020/6/1.
+ * 描述：传输传递需要自定义factory
+ */
+class DataViewModelFactory(private val counterReserved: Int) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return DataViewModel(counterReserved) as T
+    }
+}
